0aab41f3da7f3b120effaffdf5770dfc0f6f266b
elonz <zhukaihao.r@bytedance.com>
2021-05-15 19:34:02 +0800

    build: default, red-graphite
    Change-Id: Ia03a61c2beaed197a30e46578028ee65cb64f284

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ let build = () => {
     const outFile = `${distDir}/${filename.substr(0, filename.length - 5)}.css`
     try {
       console.log(`编译中：${file}`)
-      let result = sass.renderSync({ file, outFile });
+      let result = sass.renderSync({ file, outFile, sourceMap: false });
       fs.writeFile(outFile, result.css, error => {
         if (error) console.log(`写入文件失败：${outFile}`);
         else console.log(`输出：${outFile}`);
